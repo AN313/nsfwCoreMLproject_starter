8106281d973944ac126d8f237904b5ee710da6e2
Yiwei Ni <anitani313@gmail.com>
2017-08-01 14:29:45 -0400

    add content

--- nsfwDetector/ViewController.swift
+++ nsfwDetector/ViewController.swift
@@ -9,17 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+}
 
 
+// MARK: - IBActions
+extension ViewController {
+    
+    @IBAction func pickImage(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .savedPhotosAlbum
+        present(pickerController, animated: true)
+    }
+    
+    func detectPhoto(image: UIImage) {
+        answerLabel.text = "predicting..."
+        answerLabel.textAlignment = .center
+        
+        // ...
+        
+    }
+}
+
+
+// MARK: - UIImagePickerControllerDelegate
+extension ViewController: UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        dismiss(animated: true)
+        
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Can not load image from Photos")
+        }
+        
+        photo.image = image
+        detectPhoto(image: image)
+    }
+}
+
+
+// MARK: - UINavigationControllerDelegate
+extension ViewController: UINavigationControllerDelegate {
 }
 
